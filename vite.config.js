17c781ad74f98ffda9cedee3d7305ef9fd520414
AndrewR3K <10902965+AndrewR3K@users.noreply.github.com>
2024-07-07 21:01:03 -0700

    config fix

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,9 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
-export default defineConfig({\
+export default defineConfig({
   base: `/algal-bloom-map/`,
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
