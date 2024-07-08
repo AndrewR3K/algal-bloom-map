034fa9eb48e898e3e1b4f01e59591800dca294dd
AndrewR3K <10902965+AndrewR3K@users.noreply.github.com>
2024-07-08 00:19:57 -0700

    Adding basic meta

--- src/router/index.js
+++ src/router/index.js
@@ -7,19 +7,86 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      meta: {
+        title: 'Map',
+        metaTags: [
+          {
+            name: 'description',
+            content:
+              'The Algal Bloom Map is a dynamic and interactive tool that visualizes data on current algal bloom reports across California. By aggregating and presenting this information, we aim to help researchers, environmentalists, and the general public stay informed about the locations and severity of algal blooms.'
+          },
+          {
+            name: 'keywords',
+            content: 'algal, bloom, algal bloom, california, map, algal bloom map '
+          }
+        ]
+      }
     },
     {
       path: '/about',
       name: 'about',
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/AboutView.vue'),
+      meta: {
+        title: 'About',
+        metaTags: [
+          {
+            name: 'description',
+            content:
+              'The Algal Bloom Map is a dynamic and interactive tool that visualizes data on current algal bloom reports across California. By aggregating and presenting this information, we aim to help researchers, environmentalists, and the general public stay informed about the locations and severity of algal blooms.'
+          },
+          {
+            name: 'keywords',
+            content: 'algal, bloom, algal bloom, california, map, algal bloom map '
+          }
+        ]
+      }
     },
     {
       path: '/report',
       name: 'report',
-      component: () => import('../views/ReportView.vue')
+      component: () => import('../views/ReportView.vue'),
+      meta: {
+        title: 'Report',
+        metaTags: [
+          {
+            name: 'description',
+            content:
+              'The Algal Bloom Map is a dynamic and interactive tool that visualizes data on current algal bloom reports across California. By aggregating and presenting this information, we aim to help researchers, environmentalists, and the general public stay informed about the locations and severity of algal blooms.'
+          },
+          {
+            name: 'keywords',
+            content: 'algal, bloom, algal bloom, california, map, algal bloom map '
+          }
+        ]
+      }
     }
   ]
+})
+
+router.beforeResolve((to, from) => {
+  document.title = (to.meta.title || 'Algal Bloom Map') + ' | Algal Bloom Map'
+  if (to.meta.hasOwnProperty('metaTags')) {
+    for (let x = 0; x < to.meta.metaTags.length; x++) {
+      let oldMeta = document.getElementsByTagName('meta')
+      for (let m = 0; m < oldMeta.length; m++) {
+        if (oldMeta[m].name == to.meta.metaTags[x].name) {
+          oldMeta[m].remove()
+        }
+      }
+      let meta = document.createElement('meta')
+      if (to.meta.metaTags[x] && to.meta.metaTags[x].hasOwnProperty('name')) {
+        meta.name = to.meta.metaTags[x].name
+        meta.content = to.meta.metaTags[x].content
+      }
+      if (to.meta.metaTags[x].hasOwnProperty('property')) {
+        meta.property = to.meta.metaTags[x].name
+        meta.content = to.meta.metaTags[x].content
+      }
+      document.head.appendChild(meta)
+    }
+  }
+  // next()
 })
 
 export default router
