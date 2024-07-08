dee06698d5c4b61b3bcca356a7a64dda60d18ddd
AndrewR3K <10902965+AndrewR3K@users.noreply.github.com>
2024-07-07 20:03:13 -0700

    deployment change

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig({\
+  base: `/algal-bloom-map/`,
   plugins: [
     vue(),
     vueDevTools(),
