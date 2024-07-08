59130231db4b34075257b505018bb3cf53c5ce9b
AndrewR3K <10902965+AndrewR3K@users.noreply.github.com>
2024-07-08 01:18:57 -0700

    about page style fix

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,5 @@
 <template>
-  <main
-    class="text-gray-500 dark:text-gray-100 font-sans mt-16"
-    style="min-height: calc(100vh - 220px)"
-  >
+  <main class="text-gray-500 dark:text-gray-100 font-sans mt-16" style="min-height: calc(100vh)">
     <div class="max-w-4xl mx-auto py-10 px-6 bg-white dark:bg-neutral-600 shadow-md rounded-md">
       <h1 class="text-3xl font-bold mb-4">About Algal Bloom Map</h1>
       <p class="mb-6">
